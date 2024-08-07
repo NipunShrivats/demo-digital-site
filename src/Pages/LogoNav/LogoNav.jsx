d39f2d414e8962cef1433f9f69668ccdf4f520cb
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-07 16:53:17 +0530

    new changes last on 7thAug 8

--- src/Pages/LogoNav/LogoNav.jsx
+++ src/Pages/LogoNav/LogoNav.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import logo from "../../assets/imgAssets/ssLogo.png"
 import "./LogoNavStyle.css"
-
+import { Link } from "react-router-dom"
 
 export default function Nav() {
     return (
         <>
             <div className='nav'>
-                <a href="/"><img src={logo} alt="" /></a>
+                <Link to="/"><img src={logo} alt="" /></Link>
             </div>
         </>
     )
