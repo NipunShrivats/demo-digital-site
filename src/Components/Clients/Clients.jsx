5144cfd2e94bcf2d6d47f6a88c0f36ce50db3126
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-10 14:03:07 +0530

    new changes last on 10thAug change5

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,95 +1,53 @@
 import React, { useRef } from 'react'
 import './ClientsStyle.css'
-import { FaArrowRight } from "react-icons/fa";
-import { FaArrowLeft } from "react-icons/fa";
 import { Link } from "react-router-dom"
 
-import client1 from "../../assets/imgAssets/client1.jpg"
-import client2 from "../../assets/imgAssets/client2.jpg"
-import client3 from "../../assets/imgAssets/client3.jpg"
-import client4 from "../../assets/imgAssets/client4.jpg"
+import client1 from "../../assets/clientHandles/client1.jpg"
+import client2 from "../../assets/clientHandles/client2.jpg"
+import client3 from "../../assets/clientHandles/client3.jpg"
+import client4 from "../../assets/clientHandles/client4.jpg"
 
-import leftBtn from "../../assets/imgAssets/left-arrow.png";
-import rightBtn from "../../assets/imgAssets/right-arrow.png";
-
-function InsideSlider1(props) {
-    const { clientImage, userName, userLocation, userReview } = props
-    return (
-        <li>
-            <div className='slide-c'>
-                <div className='user-info-c'>
-                    <img src={clientImage} alt="" />
-                    <div className='text-info'>
-                        <h3>{userName}</h3>
-                        <span>{userLocation}</span>
-                    </div>
-                </div>
-                <p>{userReview}</p>
-            </div>
-        </li>
-    )
-}
-
+import Card from '../../Components/Card/Card'
+// function InsideSlider1(props) {
+//     const { clientImage, userName, userLocation, userReview } = props
+//     return (
+//         <li>
+//             <div className='slide-c'>
+//                 <div className='user-info-c'>
+//                     <img src={clientImage} alt="" />
+//                     <div className='text-info'>
+//                         <h3>{userName}</h3>
+//                         <span>{userLocation}</span>
+//                     </div>
+//                 </div>
+//                 <p>{userReview}</p>
+//             </div>
+//         </li>
+//     )
+// }
 
 export default function Clients() {
-
-    const slider = useRef();
-    let tx = 0;
-
-    const slidePrev = () => {
-        if (tx < 0) {
-            tx += 25;
-        }
-        slider.current.style.transform = `translateX(${tx}%)`;
-    }
-
-    const slideNext = () => {
-        if (tx > -50) {
-            tx -= 25;
-        }
-        slider.current.style.transform = `translateX(${tx}%)`;
-    }
-
     return (
         <>
             <div className='clients'>
-                {/* buttons */}
-                {/* <button className='prev-btn-c' onClick={slidePrev}><FaArrowLeft /></button>
-                <button className='next-btn-c' onClick={slideNext}><FaArrowRight /></button> */}
-
-                <button className='prev-btn-c' onClick={slidePrev}><img src={leftBtn} alt="" /></button>
-                <button className='next-btn-c' onClick={slideNext}><img src={rightBtn} alt="" /></button>
-
-                <div className='slider-c'>
-                    <ul ref={slider}>
-                        <InsideSlider1
-                            clientImage={client1}
-                            userName={"Suchita Joshi"}
-                            userLocation={"@p5yoga_wellness"}
-                            userReview={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores ipsa, reprehenderit error dolores, minima ad quasi consectetur fugiat, minus ratione aliquam placeat repellendus. Cumque provident asperiores autem tempore suscipit?"}
-                        />
-                        <InsideSlider1
-                            clientImage={client2}
-                            userName={"Durvam's Diary"}
-                            userLocation={"@durvasumedha"}
-                            userReview={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores ipsa, reprehenderit error dolores, minima ad quasi consectetur fugiat, minus ratione aliquam placeat repellendus. Cumque provident asperiores autem tempore suscipit?"}
-                        />
-                        <InsideSlider1
-                            clientImage={client3}
-                            userName={"Glance Fire & Safety consultant"}
-                            userLocation={"@glanceenterprice"}
-                            userReview={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores ipsa, reprehenderit error dolores, minima ad quasi consectetur fugiat, minus ratione aliquam placeat repellendus. Cumque provident asperiores autem tempore suscipit?"}
-                        />
-                        <InsideSlider1
-                            clientImage={client4}
-                            userName={"Sam Bahadur"}
-                            userLocation={"Patiyala, Punjab"}
-                            userReview={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores ipsa, reprehenderit error dolores, minima ad quasi consectetur fugiat, minus ratione aliquam placeat repellendus. Cumque provident asperiores autem tempore suscipit?"}
-                        />
-                    </ul>
+                <div className='clientBox1'>
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
                 </div>
-
-                <button className='btn dark-btn view-more'><Link to="/clients/Allclients">View More clients</Link></button>
+                <button className='btn dark-btn'><Link to={"/clients/Allclients"}>View More Clients</Link> </button>
             </div>
         </>
     )
--- src/Pages/Allclients/Allclients.jsx
+++ src/Pages/Allclients/Allclients.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import "./AllClientsStyle.css"
+
 import Nav from '../../Pages/LogoNav/LogoNav'
+import Card from '../../Components/Card/Card'
+
 export default function Allclients() {
     return (
         <>
             <Nav />
+            <div className='container AllClients'>
+                <h1 className='heading'>View More Clients</h1>
+
+                <div className='clientBox'>
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                    <Card
+                        firstAlphabet={"A"}
+                        name={"Name"}
+                        username={"Username"}
+                        image={""} />
+                </div>
+            </div>
         </>
     )
 }
