12a2994d124a1f2c040b97603a7ff03c30d33c56
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-10 15:13:35 +0530

    new changes last on 10thAug change9

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,44 +1,19 @@
 import * as React from 'react';
 import "./CardStyle.css"
-
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Link } from "react-router-dom";
-
 import { RiInstagramFill } from "react-icons/ri";
 
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
 
 export default function RecipeReviewCard(props) {
 
     const { firstAlphabet, name, username, image, to } = props;
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
 
     return (
         <Card sx={{ maxWidth: 345 }}>
