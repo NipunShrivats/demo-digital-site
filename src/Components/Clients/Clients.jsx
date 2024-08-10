89cc46c66204ea9b3cd636dea885569bae2d9e5c
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-10 15:44:48 +0530

    new changes last on 10thAug change15

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -8,24 +8,6 @@ import client3 from "../../assets/clientHandles/client3.jpg"
 import client4 from "../../assets/clientHandles/client4.jpg"
 
 import Card from '../../Components/Card/Card'
-// import { TbBackground } from 'react-icons/tb'
-// function InsideSlider1(props) {
-//     const { clientImage, userName, userLocation, userReview } = props
-//     return (
-//         <li>
-//             <div className='slide-c'>
-//                 <div className='user-info-c'>
-//                     <img src={clientImage} alt="" />
-//                     <div className='text-info'>
-//                         <h3>{userName}</h3>
-//                         <span>{userLocation}</span>
-//                     </div>
-//                 </div>
-//                 <p>{userReview}</p>
-//             </div>
-//         </li>
-//     )
-// }
 
 export default function Clients() {
     return (
--- src/Pages/Allclients/Allclients.jsx
+++ src/Pages/Allclients/Allclients.jsx
@@ -1,46 +1,13 @@
 import React from 'react'
 import "./AllClientsStyle.css"
 
-import Nav from '../../Pages/LogoNav/LogoNav'
-import Card from '../../Components/Card/Card'
+import Nav from "../LogoNav/LogoNav";
 
-import client1 from "../../assets/clientHandles/client1.jpg"
-import client2 from "../../assets/clientHandles/client2.jpg"
-import client3 from "../../assets/clientHandles/client3.jpg"
-import client4 from "../../assets/clientHandles/client4.jpg"
-
-
-export default function Allclients() {
+export default function AllClients() {
     return (
         <>
             <Nav />
-            <div className='container AllClients'>
-                <h1 className='heading'>View More Clients</h1>
-
-                <div className='clientBox'>
-                    <Card
-                        firstAlphabet={"S"}
-                        name={"Shveta Sharma"}
-                        username={"@ahaana_holistichealing"}
-                        image={client1}
-                        to={"https://www.instagram.com/ahaana_holistichealing/"}
-                    />
-                    <Card
-                        firstAlphabet={"N"}
-                        name={"Naira Sri"}
-                        username={"@cutiekuhoo"}
-                        image={client2}
-                        to={"https://www.instagram.com/cutiekuhoo/"}
-                    />
-                    <Card
-                        firstAlphabet={"V"}
-                        name={"Virender Sharma"}
-                        username={"@virendersharmababa_ji"}
-                        image={client3}
-                        to={"https://www.instagram.com/virendersharmababa_ji/?hl=en"}
-                    />
-                </div>
-            </div>
+            <div>AllClients</div>
         </>
     )
 }
