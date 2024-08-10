d036a0ccc88dbef8a945da6055f9ec5d645049c6
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-10 15:52:36 +0530

    new changes last on 10thAug change17

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import VideoEditing from './Pages/AllServices/VideoEditing/VideoEditing'
 import SocialMediaManagement from './Pages/AllServices/SocialMediaManagement/SocialMediaManagement'
 import BrandPromotion from './Pages/AllServices/BrandPromotion/BrandPromotion'
 // import AllClients from "./Pages/AllClients/AllClients";
+import Data from "./Pages/Data/Data";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -27,6 +28,7 @@ export default function App() {
           <Route path='/services/videoediting' exact={true} element={<VideoEditing />} />
           <Route path='/services/socialmediamanagement' exact={true} element={<SocialMediaManagement />} />
           <Route path='/services/brandpromotion' exact={true} element={<BrandPromotion />} />
+          <Route path='/Data' exact={true} element={<Data />} />
           {/* <Route path='/clients/AllClients' exact={true} element={<AllClients />} /> */}
         </Routes>
         <div className='container'>
