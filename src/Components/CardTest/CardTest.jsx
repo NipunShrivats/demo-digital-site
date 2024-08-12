c74ae06476e7ba304dbeb82db48ebe24c69a71db
Nipun Shrivats <nipunrawat16@gmail.com>
2024-08-12 11:35:08 +0530

    new changes last on 12thAug change2

--- src/Components/CardTest/CardTest.jsx
+++ src/Components/CardTest/CardTest.jsx
@@ -47,7 +47,7 @@ export default function CardTest(props) {
                 <Typography variant="body2">
                     {test} <br />
                 </Typography>
-                <span className='quote-name'>--{name}</span>
+                <span className='quote-name'>— {name}</span>
                 <span className='coma comaR'><RiDoubleQuotesR /></span>
 
             </CardContent>
